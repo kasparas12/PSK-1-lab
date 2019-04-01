80df99911d918cf1ca6564dba163cd536460b1b9
Kasparas Taminskas <kasparas.taminskas@yahoo.lt>
2019-04-01 23:12:12 +0300

    H2 database connected, added Player as entity

--- src/main/java/vu/mif/entities/Player.java
+++ src/main/java/vu/mif/entities/Player.java
@@ -1,13 +1,23 @@
 package vu.mif.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
+@Table(name="PLAYER")
 public class Player implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max=50)
+    @Column(name = "NAME")
     private String name;
 
-    public Player() {
-
-    }
+    public Player() { }
 
     public Player(String name) {
         this.name = name;
@@ -19,5 +29,26 @@ public class Player implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player)o;
+        return Objects.equals(id, player.id) && Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
